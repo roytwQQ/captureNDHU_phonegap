506653a0c3a1fd44b6c67a6bd21565ea9a3a236c
Roy <roysashimi@gmail.com>
2014-09-27 20:35:44 +0800

    iOS init

---
+++ platforms/ios/www/js/mobileinit.js
@@ -0,0 +1,14 @@
+	var onLoad = function () {
+        document.addEventListener("deviceready", onDeviceReady, false);
+        document.addEventListener("resume", onResume, false);
+    };
+    
+    var onDeviceReady = function () {
+    	alert("onDeviceReady");
+    };
+    
+   
+    
+    var onResume = function () {
+    	alert("on resume");
+    };
